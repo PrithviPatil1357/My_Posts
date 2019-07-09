3de6fc5f3e33459037d077508f173b0cceeb04a7
Prithvi Patil <prithvipatil1357@gmail.com>
2019-07-09 22:25:22 +0530

    without update functionality

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const mongodb = require('mongodb');
-
+const mongoURL=require("../../../mongo");
+//Get Posts
 router.get('/', async (req, res) => {
   const posts = await loadPostsCollection();
   res.send(await posts.find({}).toArray());
+   
 });
 
-//Add post
 
+//Add post
 router.post('/',async (req,res)=>{
   const posts = await loadPostsCollection();
   await posts.insertOne({
     text: req.body.text,
-    createdAt: new Date(),
-
+    createdAt: new Date()
   });
   res.status(201).send();
 })
 
 //Delete post
-
 router.delete('/:id',async (req,res)=>{
   const posts = await loadPostsCollection();
   await posts.deleteOne({_id:new mongodb.ObjectID(req.params.id)});
@@ -30,7 +30,7 @@ router.delete('/:id',async (req,res)=>{
 async function loadPostsCollection() {
   
   const client = await mongodb.MongoClient.connect(
-    'mongodb://Prithvi123:Prithvi1@tmcluster-shard-00-00-jwcyt.mongodb.net:27017,tmcluster-shard-00-01-jwcyt.mongodb.net:27017,tmcluster-shard-00-02-jwcyt.mongodb.net:27017/test?ssl=true&replicaSet=TMCluster-shard-0&authSource=admin&retryWrites=true&w=majority',
+    mongoURL,
     {
       useNewUrlParser: true
     }
